787dfd865699031d4f8fbdceca71856b052f1029
Iurii Zozulynskyi <iurii.zozulynskyi@gmail.com>
2020-06-06 22:20:49 +0400

    added stop and reset button, added background

--- js/script.js
+++ js/script.js
@@ -2,71 +2,54 @@ const ADD_ZERO_TO_TIME = 10;
 const MILLISECONDS_IN_SECOND = 1000;
 const SECONDS_IN_MINUTE = 60;
 
-let currentDate, currentHours, currentMinutes, currentSeconds, currentTime;
-
 const container = document.getElementById('container');
 
-setInterval(() => {
-    currentDate = new Date();
-    currentHours = currentDate.getHours();
-    currentMinutes = currentDate.getMinutes();
-    currentSeconds = currentDate.getSeconds();
-    if (currentSeconds < ADD_ZERO_TO_TIME) {
-        currentSeconds = '0' + currentSeconds;
-    }
-    if (currentMinutes < ADD_ZERO_TO_TIME) {
-        currentMinutes = '0' + currentMinutes;
-    }
-    if (currentHours < ADD_ZERO_TO_TIME) {
-        currentHours = '0' + currentHours;
-    }
-    currentTime = `${currentHours}:${currentMinutes}:${currentSeconds}`;
+addClock()
+addTimerMenu()
 
+function addClock () {
+    let currentDate, currentHours, currentMinutes, currentSeconds, currentTime;
     const div = document.createElement('div');
     div.setAttribute('id', 'current-time');
-    div.textContent = `${currentTime}`;
-    if (container.firstElementChild) {
-        container.firstElementChild.replaceWith(div)
-    } else {
-        container.append(div);
-    }  
-}, MILLISECONDS_IN_SECOND);
+    div.textContent = 'Loading...'    
+    container.append(div)
+    setInterval(() => {
+        currentDate = new Date();
+        currentTime = currentDate.toLocaleTimeString('it-IT');
+        div.textContent = `${currentTime}`;
+    }, MILLISECONDS_IN_SECOND);
+};
 
-setTimeout(() => {
+function addTimerMenu () {
     const div = document.createElement('div');
     div.setAttribute('id', 'timer');
     const label = document.createElement('label');
     label.textContent = 'Set timer to:   ';
     const input = document.createElement('input');
-    // input.setAttribute('type', 'numbers');
-    input.placeholder = 'Minutes'
+    input.placeholder = 'Minutes';
     const startButton = document.createElement('button');
-    startButton.setAttribute('onclick', 'setTimer()');
-    startButton.textContent = 'Start'
+    startButton.setAttribute('onclick', 'startTimer()');
+    startButton.textContent = 'Start';
     const stopButton = document.createElement('button');
     stopButton.setAttribute('onclick', 'stopTimer()');
-    stopButton.textContent = 'Stop'
-    div.append(label, input, startButton, stopButton);
+    stopButton.textContent = 'Stop';
+    const resetButton = document.createElement('button');
+    resetButton.setAttribute('onclick', 'resetTimer()');
+    resetButton.textContent = 'Reset';
+    div.append(label, input, startButton, stopButton, resetButton);
     container.append(div);
-}, MILLISECONDS_IN_SECOND);
+};
 
-function setTimer () {
+function startTimer () {
     let interval = document.querySelector('input').value;
-    const miliseconds = interval * SECONDS_IN_MINUTE * MILLISECONDS_IN_SECOND;
-    timer(miliseconds);
-    setTimeout(() => {
-        let audio = new Audio();
-        audio.src = './Assets/ring.mp3';
-        audio.autoplay = true;
-    }, miliseconds);
-}
+    const secondInterval = interval * SECONDS_IN_MINUTE || interval.split(':')[0] * SECONDS_IN_MINUTE + +interval.split(':')[1];
+    const milisecondsInterval = secondInterval * MILLISECONDS_IN_SECOND;
+    timer(milisecondsInterval);
+};
 
 function timer (miliseconds) {
     let timeLeft = miliseconds;
-    let secondsLeft, minutesLeft, leftTime;
-    const div = document.createElement('div');
-    container.append(div);
-    const interval = setInterval(() => {
+    window.timerTickTac = setInterval(() => {
         secondsLeft = timeLeft / MILLISECONDS_IN_SECOND % SECONDS_IN_MINUTE;
         minutesLeft = (timeLeft / MILLISECONDS_IN_SECOND - secondsLeft) / SECONDS_IN_MINUTE;
         if (secondsLeft < ADD_ZERO_TO_TIME) {
@@ -76,23 +59,22 @@ function timer (miliseconds) {
             minutesLeft = '0' + minutesLeft;
         }
         leftTime = `${minutesLeft}:${secondsLeft}`;
-        document.querySelector('input').value = leftTime;
-        // const div = document.createElement('div');
-        // div.setAttribute('id', 'left-time');
-        // div.textContent = `${leftTime}`;
-        // if (container.lastElementChild) {
-        //     container.lastElementChild.replaceWith(div)
-        // } else {
-        //     container.append(div);
-        // }  
         timeLeft -= MILLISECONDS_IN_SECOND;
+        document.querySelector('input').value = leftTime;
         if (timeLeft < 0) {
-            clearInterval(interval);
+            let audio = new Audio();
+            audio.src = './Assets/ring.mp3';
+            audio.autoplay = true;
+            clearInterval(window.timerTickTac);
         }
     }, MILLISECONDS_IN_SECOND);
+};
 
-}
+function stopTimer () {
+    clearInterval(window.timerTickTac);
+};
 
-// function stopTimer () {
-
-// }
+function resetTimer () {
+    clearInterval(window.timerTickTac);
+    document.querySelector('input').value = '';
+};
